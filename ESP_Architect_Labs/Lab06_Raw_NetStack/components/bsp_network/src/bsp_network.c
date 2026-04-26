b60adaeccd218205e0935ecbc0fac30b825cf4f0
ChineseLJH <160102672+ChineseLJH@users.noreply.github.com>
2026-04-26 16:42:30 +0800

    移除不必要的 FreeRTOS 头文件引用

--- ESP_Architect_Labs/Lab06_Raw_NetStack/components/bsp_network/src/bsp_network.c
+++ ESP_Architect_Labs/Lab06_Raw_NetStack/components/bsp_network/src/bsp_network.c
@@ -1,12 +1,6 @@
 #include <string.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "freertos/event_groups.h"
-
 #include "nvs_flash.h"
 #include "esp_wifi.h"
-#include "esp_event.h"
-#include "esp_netif.h"
 #include "esp_log.h"
 
 #include "bsp_network.h"
